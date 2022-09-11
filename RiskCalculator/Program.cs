1eb0db364f64cb3fee9dd109b332040d83b151a9
David Baker <daveonplanetearth@gmail.com>
2022-09-11 18:30:53 +0100

    Added RiskCalculator solution

---
+++ RiskCalculator/Program.cs
@@ -0,0 +1,14 @@
+﻿using RiskCalculator;
+
+// See https://aka.ms/new-console-template for more information
+
+Calculator calc = new Calculator();
+
+string accountCurrency = args[0].ToUpper();
+string pair = args[1].ToUpper();
+double pips = double.Parse(args[2]);
+double risk = calc.CalcRisk(accountCurrency, pair, pips, double.Parse(args[3]));
+double accountSize = double.Parse(args[4]);
+Console.WriteLine($"Risk for {pair} {pips} pips is: {risk.ToString("F2")}{accountCurrency} A/C risk: {((risk/accountSize)*100).ToString("F2")}%");
+
+// Console.ReadLine();
---
+++ RiskCalculator/RIskCalculator.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiskCalculator
+{
+    public class Calculator
+    {
+        class FxPair
+        {
+            internal string Pair;
+            internal double Rate;
+        }
+
+        List<FxPair> fxPairs = new List<FxPair>();
+
+        public Calculator()
+        {
+            //String[] pairs = File.ReadAllLines(@"C:\Users\mtaylor\Dropbox\MT4\FxRates.txt");
+
+            //foreach (string p in pairs)
+            //{
+            //    string[] columnData = p.Split('\t');
+            //    FxPair pair = new FxPair() { Pair = columnData[0], Rate = double.Parse(columnData[1]) };
+            //    fxPairs.Add(pair);
+            //}
+
+            ReadExchangeRatesFromAzure();
+        }
+
+        double GetExchangeRateForAccountCurrency(string accountCurrency, string pair)
+        {
+            double rate = 1;
+            string quote = GetQuote(pair);
+            string quoteBase = (quote + accountCurrency).ToUpper();
+
+            if (accountCurrency != quote)
+            {
+                FxPair foundPair = (from fx in fxPairs
+                                    where fx.Pair == quoteBase
+                                    select fx).FirstOrDefault();
+
+                rate = foundPair.Rate;
+            }
+
+            return rate;
+        }
+
+        string GetQuote(string pair)
+        {
+            string quote = pair.Substring(3, 3);
+
+            if (quote == "CNH")
+                quote = "CNY";
+
+            return quote;
+        }
+
+        byte GetNumberOfDigits(string quote)
+        {
+            if ((quote == "JPY") || (quote == "HUF") || (quote == "THB"))
+                return 3;
+            else
+                return 5;
+        }
+
+        public double CalcRisk(string accountCurrency,
+            string pair,
+            double pips,
+            double lotSizeInUnits)
+        {
+            double rate = GetExchangeRateForAccountCurrency(accountCurrency, pair);
+            string quote = GetQuote(pair);
+            byte digits = GetNumberOfDigits(quote);
+
+            if (digits == 5)
+                pips = (pips * 0.0001);
+            else
+                pips = (pips * 0.01);
+
+            double risk = pips * rate * lotSizeInUnits;
+            return Math.Round(risk, 2);
+        }
+
+        //public void UploadExchangeRatesToAzure()
+        //{
+        //    CloudStorageAccount storageAccount = Common
+        //        .CreateStorageAccountFromConnectionString(CloudConfigurationManager.GetSetting("StorageConnectionString"));
+        //    CloudFileClient cloudFileClient = storageAccount.CreateCloudFileClient();
+        //    CloudFileShare cloudFileShare = cloudFileClient.GetShareReference("exchangerates");
+        //    CloudFileDirectory rootDirectory = cloudFileShare.GetRootDirectoryReference();
+        //    string ratesFile = "FxRates.txt";
+        //    CloudFile cloudFile = rootDirectory.GetFileReference(ratesFile);
+        //    cloudFile.UploadFromFile(@"C:\Users\mtaylor\Dropbox\MT4\FxRates.txt");
+        //}
+
+        string ReadExchangeRatesFromAzure()
+        {
+            //CloudStorageAccount storageAccount = Common
+            //    .CreateStorageAccountFromConnectionString(CloudConfigurationManager.GetSetting("StorageConnectionString"));
+            //CloudFileClient cloudFileClient = storageAccount.CreateCloudFileClient();
+            //CloudFileShare cloudFileShare = cloudFileClient.GetShareReference("exchangerates");
+            //CloudFileDirectory rootDirectory = cloudFileShare.GetRootDirectoryReference();
+            //string ratesFile = "FxRates.txt";
+            //CloudFile cloudFile = rootDirectory.GetFileReference(ratesFile);
+
+            string contents = File.ReadAllText(@"FxRates.txt");
+
+            //using (StreamReader sr = new StreamReader(cloudFile.OpenRead(), Encoding.ASCII))
+            //{
+            //    contents = sr.ReadToEnd();
+            //}
+
+            string[] pairs = contents.Split(new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string p in pairs)
+            {
+                string[] columnData = p.Split('\t');
+                FxPair pair = new FxPair() { Pair = columnData[0], Rate = double.Parse(columnData[1]) };
+                fxPairs.Add(pair);
+            }
+
+            return contents;
+        }
+    }
+}
